1a5bf3f2ec3d398f4c7ca4bb4ab0d3e69d82e1de
De <spark49942@gmail.com>
2023-02-28 21:48:18 +0300

    ntbk-from

---
+++ edit.py
@@ -0,0 +1,8 @@
+def edit():
+    targ = input('Введите номер желаемой заметки: ')
+    targ = targ + '.'
+    with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
+        for ln in ntbk:
+            if targ in ln:
+                ntext = input('Введите заметку:\n')
+    pass
--- work.py
+++ work.py
@@ -1,15 +1,21 @@
-import view, edit, create, delete
+from view import view 
+from edit import edit 
+from create import create 
+from delete import delete
 i = 0
 print('Здравствуйте!', end=' ')
 while True:
-    menu = input('Введите желаемый пункт меню:\n(1) Показать заметки\n(2) Редактировать заметки\n(3) Создать заметку\n(4) Удалить заметку')
-    if menu == 1:
+    menu = input('Введите желаемый пункт меню:\n(1) Показать заметки\n(2) Редактировать заметки\n(3) Создать заметку\n(4) Удалить заметку\n(q) Выйти\n \n')
+    if menu == '1':
         view()
-    elif menu == 2:
+    elif menu == '2':
         edit()
-    elif menu == 3:
+    elif menu == '3':
         create()
-    elif menu == 4:
+    elif menu == '4':
         delete()
+    elif menu == 'q':
+        print('\nПриятного дня!')
+        exit()
     else:
         print('Ошибка!')
