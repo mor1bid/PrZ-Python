5851d1b2415c631b4883dd06cdebe4e5036270e2
De <spark49942@gmail.com>
2023-03-01 05:11:21 +0300

    ntbk-nonignor

--- edit.py
+++ edit.py
@@ -1,18 +1,11 @@
 def edit():
-    numbr = input('Введите номер заметки: ')
-    numbr = numbr + '.'
     targ = input('Укажите ошибку в вашей заметке: ')
     ntext = input('Введите замену: ')
     with open('ntbk.txt', 'r', encoding='UTF-8') as ntbk:
         note = ntbk.read()
-        for ln in ntbk:
-            if numbr in ln:
-                note = note.replace(targ, ntext, 1)
-        # note = note.replace(targ, ntext, 1)
-    with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
-        ntbk.write(note)
+        note = note.replace(targ, ntext, 1)
+        with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
+            ntbk.write(note)
     print('\nВыполнено!\n')
-            # elif line == '':
-            #     print('Запрошенного номера не найдено.')
     ntbk.close()
     pass
