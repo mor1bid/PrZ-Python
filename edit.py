a426ab28508b1a4dc36e1293db39f14f86d26854
De <spark49942@gmail.com>
2023-03-01 03:18:44 +0300

    ntbk-ignor

--- edit.py
+++ edit.py
@@ -1,14 +1,22 @@
 def edit():
     targ = input('Введите номер желаемой заметки: ')
     targ = targ + '.'
-    with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
-        for ln in ntbk:
-            if targ in ln:
+    with open('ntbk.txt', 'r', encoding='UTF-8') as ntbk:
+        note = ntbk.readlines()
+        for line in ntbk:
+            if targ in line:
                 ntext = input('Введите заметку:\n')
                 nln = targ + ntext
-                res = ln.replace(ln, nln)
-                ntbk.write(res)
-            elif ln == '':
+                # res = note.replace(line, nln)
+                with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
+                    ntbk.seek(0)
+                    for ln in note:
+                        if targ not in ln:
+                            ntbk.write(ln)
+                        else:
+                            ntbk.write(nln)
+                print('\nВыполнено!\n')
+            elif line == '':
                 print('Запрошенного номера не найдено.')
     ntbk.close()
     pass
