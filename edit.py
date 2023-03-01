389b1cc1b193c12071678a77a9353835e556ff4c
De <spark49942@gmail.com>
2023-03-01 04:41:33 +0300

    ntbk-ignor

--- edit.py
+++ edit.py
@@ -1,22 +1,18 @@
 def edit():
-    targ = input('Введите номер желаемой заметки: ')
-    targ = targ + '.'
+    numbr = input('Введите номер заметки: ')
+    numbr = numbr + '.'
+    targ = input('Укажите ошибку в вашей заметке: ')
+    ntext = input('Введите замену: ')
     with open('ntbk.txt', 'r', encoding='UTF-8') as ntbk:
-        note = ntbk.readlines()
-        for line in ntbk:
-            if targ in line:
-                ntext = input('Введите заметку:\n')
-                nln = targ + ntext
-                # res = note.replace(line, nln)
-                with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
-                    ntbk.seek(0)
-                    for ln in note:
-                        if targ not in ln:
-                            ntbk.write(ln)
-                        else:
-                            ntbk.write(nln)
-                print('\nВыполнено!\n')
-            elif line == '':
-                print('Запрошенного номера не найдено.')
+        note = ntbk.read()
+        for ln in ntbk:
+            if numbr in ln:
+                note = note.replace(targ, ntext, 1)
+        # note = note.replace(targ, ntext, 1)
+    with open('ntbk.txt', 'w', encoding='UTF-8') as ntbk:
+        ntbk.write(note)
+    print('\nВыполнено!\n')
+            # elif line == '':
+            #     print('Запрошенного номера не найдено.')
     ntbk.close()
     pass
